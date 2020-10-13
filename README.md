b2d7623235b7a40245f3612a0593cc2a5cb96e9f
Allan.Cop@ing.com <allan@WPU8L0048911.ad.ing.net>
2020-10-13 14:12:30 +0200

    ready for unit test creation

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # code-challenge2
+
+## Assignment 1
+
+Before you start download the file inputs.csv from <https://www.henrybeen.nl/wp-content/uploads/2020/10/001-experts-inputs.csv>​
+
+Write a program that:​
+
+Reads all inputs.csv​
+
+Writes a file outputs.csv that:​
+
+Contains the same products as from products.csv​
+
+Leaving out all products with a price below $ 10​
+
+Writes out prices in euro’s, assuming a rate of $ 1 : € 0,85​
+
+Remember,  the goal of the assignment: write an OO implementation that solves the problem, but mostly shows off your OO skills. Do not focus only solving the problem
---
+++ assignment-1/clothing.cs
@@ -0,0 +1,78 @@
+using System;
+namespace codechallenge2
+{
+    public class Clothing
+    {
+        protected int ProductId { get; set; }
+        protected string Category { get; set; }
+        protected string Description { get; set; }
+        protected double DollarPrice { get; set; }
+        protected double EuroPrice { get { return DollarPrice * 0.85; } }
+        protected string Name { get; set; }
+        public Clothing() { }
+        public Clothing(int productid, string category, string desc, int price, string name)
+        {
+            ProductId = productid;
+            Category = category;
+            Description = desc;
+            DollarPrice = price;
+            Name = name;
+        }
+        public override string ToString() =>
+        $"\"{this.ProductId}\",\"{this.Name}\",\"{this.Description}\",\"{this.EuroPrice}\",\"{this.Category}\"";
+
+        public double GetDollarPrice() => DollarPrice;
+    }
+    public class Pants : Clothing
+    {
+        // specific properties for pants
+        protected int Legs;
+        protected string category = "pants";
+        public Pants(int productid, string name, string desc, int price, int legs)
+        {
+            this.ProductId = productid;
+            this.Name = name;
+            this.Category = category;
+            this.Description = desc;
+            this.DollarPrice = price;
+            this.Legs = legs;
+        }
+
+        // Assume we have 1 leg if no legs are specified. Yarr we all be peg legged pirates.
+        public Pants(int id, string name, string desc, int price)
+        {
+            this.ProductId = id;
+            this.Name = name;
+            this.Category = category;
+            this.Description = desc;
+            this.DollarPrice = price;
+            this.Legs = 1;
+        }
+    }
+    public class Shirts : Clothing
+    {
+        // specific properties for pants
+        protected int Arms;
+        protected string category = "shirts";
+        public Shirts(int productid, string name, string desc, int price, int arms)
+        {
+            this.ProductId = productid;
+            this.Name = name;
+            this.Category = category;
+            this.Description = desc;
+            this.DollarPrice = price;
+            this.Arms = arms;
+        }
+
+        public Shirts(int id, string name, string desc, int price)
+        {
+            this.ProductId = id;
+            this.Name = name;
+            this.Category = category;
+            this.Description = desc;
+            this.DollarPrice = price;
+            this.Arms = 1;
+        }
+    }
+}
+
