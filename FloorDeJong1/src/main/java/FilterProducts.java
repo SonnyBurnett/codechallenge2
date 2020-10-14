ceec110e3ed5170dc232d6348a0a24dc944a57d7
Floor de Jong <floor.de.jong@ing.com>
2020-10-14 12:11:24 +0200

    Changed to uploading file into list

--- FloorDeJong1/src/main/java/FilterProducts.java
+++ FloorDeJong1/src/main/java/FilterProducts.java
@@ -1,11 +1,13 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class FilterProducts {
-    private ArrayList<Product> inputList;
-    private ArrayList<Product> outputList;
+    private List<Product> inputList = new ArrayList<>();
+    private List<Product> outputList;
 
     public FilterProducts(String file){
         readProductFile(file);
@@ -15,14 +17,23 @@ public class FilterProducts {
         try {
             File myObj = new File(file);
             Scanner myReader = new Scanner(myObj);
+            myReader.nextLine();
             while (myReader.hasNextLine()) {
-                String data = myReader.nextLine();
-                System.out.println(data);
+                String[] data = myReader.nextLine().split(",");
+                inputList.add(new Product(Integer.parseInt(data[0]), data[1], data[2], Double.parseDouble(data[3]), data[4]));
             }
             myReader.close();
         } catch (FileNotFoundException e) {
             System.out.println("An error occurred.");
             e.printStackTrace();
         }
+    }
+
+    public List<Product> getInputList() {
+        return inputList;
+    }
+
+    public List<Product> getOutputList() {
+        return outputList;
     }
 }
