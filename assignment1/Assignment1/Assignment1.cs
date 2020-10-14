6cf4bb8c5b2fc81fb7106e1fcaa28fb4b3e97c38
Allan.Cop@ing.com <allan@WPU8L0048911.ad.ing.net>
2020-10-14 14:45:33 +0200

    cleanup of code.

--- assignment1/Assignment1/Assignment1.cs
+++ assignment1/Assignment1/Assignment1.cs
@@ -1,45 +1,49 @@
-﻿using System;
-using System.Linq;
-using System.Collections.Generic;
+﻿// Assignment 1 for the code challenge 2
+
 namespace Assignment1.Services
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
     public static class Assignment1
     {
         public static void Main(string[] args)
         {
-            int max_price = 10;
+            int max_dollar_price = 10;
             string outputfile = "outputs.csv";
             string inputfile = "inputs.csv";
+            string inputpath = $"{inputfile}";
+            string outputpath = $"{outputfile}";
+            string[] input_lines = System.IO.File.ReadAllLines(path: inputpath);
 
-            string[] lines = System.IO.File.ReadAllLines(@$"{ inputfile }");
             // store headers for later
-            string headers = lines[0];
+            string headers = input_lines[0];
+
             // remove headers from array.
-            lines = lines.Skip(1).ToArray();
-            List<Clothing> ClothingRecords = new List<Clothing>();
-            foreach (string line in lines)
-            {
-                ClothingRecords.Add(ClothingFactory(line.Split(",")));
-            }
+            input_lines = input_lines.Skip(1).ToArray();
+            List<Clothing> clothingRecords = (from string input_line in input_lines
+                                              select ClothingFactory(input_line.Split(","))).ToList();
             using (System.IO.StreamWriter file =
-            new System.IO.StreamWriter(@$"{ outputfile }"))
+            new System.IO.StreamWriter(path: outputpath))
             {
                 file.WriteLine(headers);
-                foreach (Clothing clothing in ClothingRecords)
+                foreach (Clothing clothing in clothingRecords)
                 {
-                    if (clothing.GetDollarPrice() >= max_price)
+                    if (clothing.GetDollarPrice() >= max_dollar_price)
                     {
                         file.WriteLine(clothing.ToString());
                     }
                 }
             }
         }
+
         public static Clothing ClothingFactory(string[] clothingdescription)
         {
-            int productId = Int32.Parse(clothingdescription[0]);
+            var productId = int.Parse(clothingdescription[0]);
             string name = clothingdescription[1].Trim();
             string desc = clothingdescription[2].Trim();
-            int price = Int32.Parse(clothingdescription[3]);
+            int price = int.Parse(clothingdescription[3]);
             string category = clothingdescription[4].Trim();
 
             if (category == "pants")
@@ -56,5 +60,4 @@ namespace Assignment1.Services
             }
         }
     }
-
 }
--- assignment1/Assignment1/clothing.cs
+++ assignment1/Assignment1/clothing.cs
@@ -1,80 +1,138 @@
-using System;
+// Contains Clothing classes used by Assignment 1
+
 namespace Assignment1.Services
 {
+    using System;
+
     public class Clothing
     {
-        protected int ProductId { get; set; }
+        protected int ProductId
+        {
+            get;
+            set;
+        }
 
         // protected Boolean Sex {get; set; } - No longer allowed.
-        protected string Category { get; set; }
-        protected string Description { get; set; }
-        protected double DollarPrice { get; set; }
-        protected double EuroPrice { get { return DollarPrice * 0.85; } }
-        protected string Name { get; set; }
-        public Clothing() { }
-        public Clothing(int productid, string category, string desc, int price, string name)
+        protected string Category
         {
-            ProductId = productid;
-            Category = category;
-            Description = desc;
-            DollarPrice = price;
-            Name = name;
+            get;
+            set;
         }
-        public override string ToString() =>
-        $"\"{this.ProductId}\",\"{this.Name}\",\"{this.Description}\",\"{this.EuroPrice}\",\"{this.Category}\"";
 
-        public double GetDollarPrice() => DollarPrice;
+        protected string Description
+        {
+            get;
+            set;
+        }
+
+        protected double DollarPrice
+        {
+            get;
+            set;
+        }
+
+        protected double EuroPrice
+        {
+            get
+            {
+                return this.DollarPrice * 0.85;
+            }
+        }
+
+        protected string Name
+        {
+            get;
+            set;
+        }
+
+        public Clothing() { }
+
+        public Clothing(
+            int productid,
+            string category,
+            string desc,
+            int price,
+            string name)
+        {
+            this.ProductId = productid;
+            this.Category = category;
+            this.Description = desc;
+            this.DollarPrice = price;
+            this.Name = name;
+        }
+
+        public override string ToString() =>
+        $"\"{this.ProductId}\"," +
+        $"\"{this.Name}\"," +
+        $"\"{this.Description}\"," +
+        $"\"{this.EuroPrice}\"," +
+        $"\"{this.Category}\"";
+
+        public double GetDollarPrice() => this.DollarPrice;
     }
+
     public class Pants : Clothing
     {
         // specific properties for pants
-        protected int Legs;
-        protected string category = "pants";
-        public Pants(int productid, string name, string desc, int price, int legs)
+        private int legs;
+        private string category = "pants";
+
+        public Pants(
+            int productid,
+            string name,
+            string desc,
+            int price,
+            int legs)
         {
             this.ProductId = productid;
             this.Name = name;
-            this.Category = category;
+            this.Category = this.category;
             this.Description = desc;
             this.DollarPrice = price;
-            this.Legs = legs;
+            this.legs = legs;
         }
 
         // Assume we have 1 leg if no legs are specified. Yarr we all be peg legged pirates.
-        public Pants(int id, string name, string desc, int price)
+        public Pants(
+            int id,
+            string name,
+            string desc,
+            int price)
         {
             this.ProductId = id;
             this.Name = name;
-            this.Category = category;
+            this.Category = this.category;
             this.Description = desc;
             this.DollarPrice = price;
-            this.Legs = 1;
+            this.legs = 1;
         }
     }
+
     public class Shirts : Clothing
     {
         // specific properties for pants
-        protected int Arms;
-        protected string category = "shirts";
+        private int arms;
+
+        private string category = "shirts";
+
         public Shirts(int productid, string name, string desc, int price, int arms)
         {
             this.ProductId = productid;
             this.Name = name;
-            this.Category = category;
+            this.Category = this.category;
             this.Description = desc;
             this.DollarPrice = price;
-            this.Arms = arms;
+            this.arms = arms;
         }
 
         public Shirts(int id, string name, string desc, int price)
         {
             this.ProductId = id;
             this.Name = name;
-            this.Category = category;
+            this.Category = this.category;
             this.Description = desc;
             this.DollarPrice = price;
-            this.Arms = 1;
+            this.arms = 1;
         }
     }
 }
-
