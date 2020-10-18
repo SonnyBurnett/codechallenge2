9a44bee4dbcae13c0d37120c38191bdbdf1b4314
Bogdan Burtea <bogdan-florian.burtea@ing.com>
2020-10-18 20:52:27 +0300

    first commit

---
+++ CodeChallenge1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CodeChallenge1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
