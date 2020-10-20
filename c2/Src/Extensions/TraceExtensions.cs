859e660fd9d2ad4ffe517c078b432c2bcca9ac9c
Mark Wildenberg <mark@teamwildenberg.nl>
2020-10-20 15:13:15 +0200

    Setup TicTacToe project structure

---
+++ c2/Src/Extensions/TraceExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace Tw.Ing.Challenge2.Extensions
+{
+    public static class TraceExtensions
+    {
+
+        public static void DoMessage(String message)
+        {
+            Trace.TraceInformation(message);
+        }
+
+        public static void DoWarn(String message)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Trace.TraceWarning(message);
+            Console.ResetColor();
+        }
+
+        public static void DoError(String message)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Trace.TraceError(message);
+            Console.ResetColor();
+        }
+
+        }
+}
---
+++ c2/Src/Program.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.Extensions.CommandLineUtils;
+using System;
+using System.Diagnostics;
+using System.Reflection;
+using System.Threading.Tasks;
+using Tw.Ing.Challenge2.Extensions;
+
+namespace Tw.Ing.Challenge2
+{
+    class Program
+    {
+        static async Task<int> Main(string[] args)
+        {
+            var tw = new TextWriterTraceListener(Console.Out);
+            tw.TraceOutputOptions |= TraceOptions.None;
+            Trace.Listeners.Add(tw);
+            Trace.AutoFlush = true;
+            Trace.Indent();
+
+            var app = new CommandLineApplication
+            {
+                Name = "TW ING Coding Challenge",
+                Description = "The TeamWildenberg console app for the different assignments in the ING Coding Challenge"
+            };
+            app.HelpOption("-?|-h|--help");
+            var versionOption = app.Option("-v|--version", "Check which version we are running", CommandOptionType.NoValue);
+
+            app.OnExecute(() =>
+            {
+                return 0;
+            });
+            app.Execute(args);
+
+            if (versionOption.HasValue())
+            {
+                var assemblyVersion = Assembly.GetEntryAssembly().GetCustomAttribute<AssemblyInformationalVersionAttribute>().InformationalVersion;
+                TraceExtensions.DoMessage($"Version: {assemblyVersion}");
+                return 0;
+            }
+            else
+            {
+                // execute command
+                try
+                {
+                    // Game Loop
+                    return 0;
+                }
+                catch (Exception)
+                {
+                    TraceExtensions.DoError($"Something fishy happened, exiting.");
+                    throw;
+                }
+            }
+        }
+    }
+}
