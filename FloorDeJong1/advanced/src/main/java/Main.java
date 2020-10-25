39f7db1c3b6af50eecf065d4fc41599616b88ab5
fndejong93 <63641336+fndejong93@users.noreply.github.com>
2020-10-25 09:48:33 +0100

    changed main and mainBeginnners

--- FloorDeJong1/advanced/src/main/java/Main.java
+++ FloorDeJong1/advanced/src/main/java/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        String input = "FloordeJong1/src/main/resources/input.csv";
+        String input = "FloordeJong1/advanced/src/main/resources/input.csv";
         ProductFilter filter = new ProductFilter();
         filter.readProductFile(input);
-        filter.createFilteredConvertedFile("FloordeJong1/src/main/resources/output.csv", 10, Currency.Type.EURO);
+        filter.createFilteredConvertedFile("FloordeJong1/advanced/src/main/resources/output.csv", 10, Currency.Type.EURO);
     }
 }
--- FloorDeJong1/beginners/src/main/java/MainBeginners.java
+++ FloorDeJong1/beginners/src/main/java/MainBeginners.java
@@ -1,9 +1,8 @@
-public class MainBeginners {
-    public static void main(String[] args) {
-        Circle circle = new Circle(1);
-        System.out.println(circle.getArea());
+import java.io.FileNotFoundException;
 
-        Square square = new Square(1);
-        System.out.println(square.getArea());
+public class MainBeginners {
+    public static void main(String[] args) throws FileNotFoundException {
+        FileProcessor processor = new FileProcessor();
+        processor.readFile("FloordeJong1/beginners/src/main/resources/001-beginners.csv");
     }
 }
