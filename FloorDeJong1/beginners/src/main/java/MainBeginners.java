4da613e37c3af85ed418a069c696dca7ceb0e265
fndejong93 <63641336+fndejong93@users.noreply.github.com>
2020-10-25 10:15:49 +0100

    Added writeArea method

--- FloorDeJong1/beginners/src/main/java/MainBeginners.java
+++ FloorDeJong1/beginners/src/main/java/MainBeginners.java
@@ -1,8 +1,9 @@
-import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class MainBeginners {
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) throws IOException {
         FileProcessor processor = new FileProcessor();
         processor.readFile("FloordeJong1/beginners/src/main/resources/001-beginners.csv");
+        processor.writeAreaFile("FloordeJong1/beginners/src/main/resources/output.csv");
     }
 }
