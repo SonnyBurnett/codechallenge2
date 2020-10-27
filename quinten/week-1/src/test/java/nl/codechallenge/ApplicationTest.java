f64fb6d3b21d4ef7a1dbf08e5bcda2850d3fdd8e
Taco Bakker <tsbakker65@gmail.com>
2020-10-27 19:39:45 +0100

    Merge project quinten as our subdirectory

---
+++ quinten/week-1/src/test/java/nl/codechallenge/ApplicationTest.java
@@ -0,0 +1,15 @@
+package nl.codechallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles("test")
+@SpringBootTest
+class ApplicationTest {
+
+    @Test
+    void contextLoads() throws Exception {
+    }
+
+}
