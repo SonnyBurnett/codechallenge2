8844df92f85b7073a251d4cc1151c898b0c6a614
Taco Bakker <tsbakker65@gmail.com>
2020-10-27 19:39:40 +0100

    Merge project bogdan as our subdirectory

---
+++ bogdan/CodeChallenge1/Products/IProduct.cs
@@ -0,0 +1,14 @@
+﻿using CodeChallenge1.Utils;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeChallenge1.Products
+{
+    interface IProduct
+    {
+        void csvToProductList(string path);
+        float convertPrice(float price, float currency);
+        void writeToCSVProductsWithPriceAbove(float minPrice);
+    }
+}
---
+++ bogdan/CodeChallenge1/Program.cs
@@ -0,0 +1,16 @@
+﻿using CodeChallenge1.Products;
+using CodeChallenge1.Utils;
+using System;
+
+namespace CodeChallenge1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductCSV productCSV = new ProductCSV();
+            productCSV.csvToProductList(@"C:\Users\YK47BU\source\repos\CodeChallenge1\CodeChallenge1\resources\001-experts-inputs.csv");
+            productCSV.writeToCSVProductsWithPriceAbove(10);
+        }
+    }
+}
