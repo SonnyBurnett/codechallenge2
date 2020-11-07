0bf55b6fa85f6d96513034f3d9dbf4c42457481f
fndejong93 <63641336+fndejong93@users.noreply.github.com>
2020-11-07 17:10:52 +0100

    Created Reader

--- FloorDeJong2/src/main/java/Reader.java
+++ FloorDeJong2/src/main/java/Reader.java
@@ -1,3 +1,29 @@
+import board.Board;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Reader {
+
+    public Board readFile(String fileLocation, Board board) throws FileNotFoundException {
+        File file = new File(fileLocation);
+
+        try (Scanner scanner = new Scanner(file)) {
+            int positionCount = 0;
+
+            while (scanner.hasNextLine()) {
+                positionCount ++;
+                String data = scanner.nextLine();
+
+                for (int i=0; i<data.length(); i++) {
+                    if (data.charAt(i)!='.') {
+                        board.setPositionValue(positionCount, String.valueOf(data.charAt(i)));
+                    }
+                }
+            }
+        }
+        return board;
+    }
 
 }
